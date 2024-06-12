4cf36a418d890df1c0c0d96e2b459a8a14af921e
anjanadhikari7 <166963730+anjanadhikari7@users.noreply.github.com>
2024-06-12 21:46:36 +1000

    pictues to be fixed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,50 @@ import projectImage5 from "../assets/project-5.png";
 import projectImage6 from "../assets/project-6.png";
 
 const ProjectsPage = () => {
-  const ProjectNumber = [1, 2, 3, 4, 5, 6];
+  const ProjectNumber = [
+    {
+      image: projectImage1,
+      github: "https://github.com/anjanadhikari7/Sales-page",
+      web: "https://anjanadhikari7.github.io/Sales-page/",
+      description: "Sales Page",
+      language: "HTML, CSS",
+    },
+    {
+      image: projectImage2,
+      github: "https://github.com/anjanadhikari7/Flashy-webpage",
+      web: "https://anjanadhikari7.github.io/Flashy-webpage/",
+      description: "Flashy Webpage",
+      language: "HTML, CSS",
+    },
+    {
+      image: projectImage3,
+      github: "https://github.com/anjanadhikari7/Travel-blog",
+      web: "https://anjanadhikari7.github.io/Travel-blog/",
+      description: "Travel Blog",
+      language: "HTML, CSS",
+    },
+    {
+      image: projectImage4,
+      github: "https://github.com/anjanadhikari7/Flexbox-Layout",
+      web: "https://anjanadhikari7.github.io/Flexbox-Layout/",
+      description: "Flexbox Model",
+      language: "HTML, CSS",
+    },
+    {
+      image: projectImage5,
+      github: "https://github.com/anjanadhikari7/Grid-Layout",
+      web: "https://anjanadhikari7.github.io/Grid-Layout/",
+      description: "Grid Layout",
+      language: "HTML, CSS",
+    },
+    {
+      image: projectImage6,
+      github: "https://github.com/anjanadhikari7/Form-basics",
+      web: "https://anjanadhikari7.github.io/Form-basics/",
+      description: "HTML Form Basics",
+      language: "HTML, CSS",
+    },
+  ];
   return (
     <section id="projects">
       <center>
@@ -16,7 +59,13 @@ const ProjectsPage = () => {
 
       <div className="flex-container">
         {ProjectNumber.map((item) => (
-          <ProjectCard />
+          <ProjectCard
+            image={item.image}
+            github={item.github}
+            web={item.web}
+            description={item.description}
+            language={item.language}
+          />
         ))}
       </div>
     </section>
